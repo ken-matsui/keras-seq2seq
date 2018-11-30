a1268e2d3f503d7d98322bec406f89dd011aef88
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-12-01 03:44:32 +0900

    WIP...

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import tensorflow as tf
-from tensorflow.python.keras import layers
+from tensorflow.python.keras.layers import *
 from tensorflow.python.keras import activations
 from tensorflow.python.keras import backend
 import numpy as np
@@ -12,17 +12,18 @@ __all__ = ['AttSeq2Seq']
 class LSTMEncoder(object):
     def __init__(self, vocab_size, embed_size, hidden_size):
         """
+        LSTM Encoder
         :param vocab_size: 使われる単語の種類数
         :param embed_size: 単語をベクトル表現した際のサイズ
         :param hidden_size: 隠れ層のサイズ
         """
         super(LSTMEncoder, self).__init__()
         # self.xe = L.EmbedID(vocab_size, embed_size, ignore_label=-1)
-        self.xe = layers.Embedding(vocab_size, embed_size, mask_zero=True)
+        self.xe = Embedding(vocab_size, embed_size, mask_zero=True)
         # self.eh = L.Linear(embed_size, 4 * hidden_size)
-        self.eh = layers.Dense(4 * hidden_size, input_shape=(embed_size,))
+        self.eh = Dense(4 * hidden_size, input_shape=(embed_size,))
         # self.hh = L.Linear(hidden_size, 4 * hidden_size)
-        self.hh = layers.Dense(4 * hidden_size, input_shape=(hidden_size,))
+        self.hh = Dense(4 * hidden_size, input_shape=(hidden_size,))
 
     def __call__(self, x, c, h):
         """
@@ -33,35 +34,35 @@ class LSTMEncoder(object):
         :return: 次の内部メモリ、次の隠れ層
         """
         with tf.variable_scope("LSTMEncoder"):
-            e = activations.tanh(self.xe(x))
-            e = layers.LSTM(c)(self.eh(e) + self.hh(h))
+            e = self.xe(x)
+            e = activations.tanh(e)
+            e = LSTM(c)(self.eh(e) + self.hh(h))
         return e
 
 
-# Attention Model + LSTM Decoder
 class AttLSTMDecoder(object):
     def __init__(self, vocab_size, embed_size, hidden_size):
         """
-        Attention ModelのためのDecoderのインスタンス化
+        Attention Model + LSTM Decoder
         :param vocab_size: 語彙数
         :param embed_size: 単語ベクトルのサイズ
         :param hidden_size: 隠れ層のサイズ
         """
         super(AttLSTMDecoder, self).__init__()
         # self.ye = L.EmbedID(vocab_size, embed_size, ignore_label=-1)  # 単語を単語ベクトルに変換する層
-        self.ye = layers.Embedding(vocab_size, embed_size, mask_zero=True)
+        self.ye = Embedding(vocab_size, embed_size, mask_zero=True)
         # self.eh = L.Linear(embed_size, 4 * hidden_size)  # 単語ベクトルを隠れ層の4倍のサイズのベクトルに変換する層
-        self.eh = layers.Dense(4 * hidden_size, input_shape=(embed_size,))
+        self.eh = Dense(4 * hidden_size, input_shape=(embed_size,))
         # self.hh = L.Linear(hidden_size, 4 * hidden_size)  # Decoderの中間ベクトルを隠れ層の4倍のサイズのベクトルに変換する層
-        self.hh = layers.Dense(4 * hidden_size, input_shape=(hidden_size,))
+        self.hh = Dense(4 * hidden_size, input_shape=(hidden_size,))
         # self.fh = L.Linear(hidden_size, 4 * hidden_size)  # 順向きEncoderの中間ベクトルの加重平均を隠れ層の4倍のサイズのベクトルに変換する層
-        self.fh = layers.Dense(4 * hidden_size, input_shape=(hidden_size,))
+        self.fh = Dense(4 * hidden_size, input_shape=(hidden_size,))
         # self.bh = L.Linear(hidden_size, 4 * hidden_size)  # 順向きEncoderの中間ベクトルの加重平均を隠れ層の4倍のサイズのベクトルに変換する層
-        self.bh = layers.Dense(4 * hidden_size, input_shape=(hidden_size,))
+        self.bh = Dense(4 * hidden_size, input_shape=(hidden_size,))
         # self.he = L.Linear(hidden_size, embed_size)  # 隠れ層サイズのベクトルを単語ベクトルのサイズに変換する層
-        self.he = layers.Dense(embed_size, input_shape=(hidden_size,))
+        self.he = Dense(embed_size, input_shape=(hidden_size,))
         # self.ey = L.Linear(embed_size, vocab_size)  # 単語ベクトルを語彙数サイズのベクトルに変換する層
-        self.ey = layers.Dense(vocab_size, input_shape=(embed_size,))
+        self.ey = Dense(vocab_size, input_shape=(embed_size,))
 
     def __call__(self, y, c, h, f, b):
         """
@@ -77,28 +78,29 @@ class AttLSTMDecoder(object):
             # 単語を単語ベクトルに変換
             e = activations.tanh(self.ye(y))
             # 単語ベクトル、Decoderの中間ベクトル、順向きEncoderのAttention、逆向きEncoderのAttentionを使ってLSTM
-            c, h = layers.LSTM(c)(self.eh(e) + self.hh(h) + self.fh(f) + self.bh(b))
+            c, h = LSTM(c)(self.eh(e) + self.hh(h) + self.fh(f) + self.bh(b))
             # LSTMから出力された中間ベクトルを語彙数サイズのベクトルに変換する
             t = self.ey(activations.tanh(self.he(h)))
         return t, c, h
 
 
 class Attention(object):
-    def __init__(self, hidden_size):
+    def __init__(self, hidden_size, batch_size):
         """
         Attentionのインスタンス化
         :param hidden_size: 隠れ層のサイズ
         """
         super(Attention, self).__init__()
         self.hidden_size = hidden_size
-        # self.fh = L.Linear(hidden_size, hidden_size)  # 順向きのEncoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
-        self.fh = layers.Dense(hidden_size, input_shape=(hidden_size,))
-        # self.bh = L.Linear(hidden_size, hidden_size)  # 逆向きのEncoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
-        self.bh = layers.Dense(hidden_size, input_shape=(hidden_size,))
-        # self.hh = L.Linear(hidden_size, hidden_size)  # Decoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
-        self.hh = layers.Dense(hidden_size, input_shape=(hidden_size,))
-        # self.hw = L.Linear(hidden_size, 1)  # 隠れ層サイズのベクトルをスカラーに変換するための線形結合層
-        self.hw = layers.Dense(1, input_shape=(hidden_size,))
+        self.batch_size = batch_size
+        # 順向きのEncoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
+        self.fh = Dense(hidden_size, input_shape=(hidden_size,))
+        # 逆向きのEncoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
+        self.bh = Dense(hidden_size, input_shape=(hidden_size,))
+        # Decoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
+        self.hh = Dense(hidden_size, input_shape=(hidden_size,))
+        # 隠れ層サイズのベクトルをスカラーに変換するための線形結合層
+        self.hw = Dense(1, input_shape=(hidden_size,))
 
     def __call__(self, fs, bs, h):
         """
@@ -108,10 +110,9 @@ class Attention(object):
         :param h: Decoderで出力された中間ベクトル
         :return: 順向きのEncoderの中間ベクトルの加重平均と逆向きのEncoderの中間ベクトルの加重平均
         """
-        batch_size = h.data.shape[0]
         # weight
         ws = []
-        sum_w = np.zeros((batch_size, 1), dtype='float32')  # ウェイトの合計値を計算するための値を初期化
+        sum_w = backend.zeros((self.batch_size, 1))  # ウェイトの合計値を計算するための値を初期化
         # Encoderの中間ベクトルとDecoderの中間ベクトルを使ってウェイトの計算
         for f, b in zip(fs, bs):
             # 順向きEncoderの中間ベクトル、逆向きEncoderの中間ベクトル、Decoderの中間ベクトルを使ってウェイトの計算
@@ -120,38 +121,39 @@ class Attention(object):
             ws.append(w)  # 計算したウェイトを記録
             sum_w += w
         # 出力する加重平均ベクトルの初期化
-        att_f = np.zeros((batch_size, self.hidden_size), dtype='float32')
-        att_b = np.zeros((batch_size, self.hidden_size), dtype='float32')
+        att_f = backend.zeros((self.batch_size, self.hidden_size))
+        att_b = backend.zeros((self.batch_size, self.hidden_size))
         for f, b, w in zip(fs, bs, ws):
             w /= sum_w  # ウェイトの和が1になるように正規化
             # ウェイト * Encoderの中間ベクトルを出力するベクトルに足していく
-            att_f += layers.Reshape((batch_size, self.hidden_size))(backend.batch_dot(f, w))
-            att_b += layers.Reshape((batch_size, self.hidden_size))(backend.batch_dot(b, w))
+            att_f += Reshape((self.batch_size, self.hidden_size))(backend.batch_dot(f, w))
+            att_b += Reshape((self.batch_size, self.hidden_size))(backend.batch_dot(b, w))
         return att_f, att_b
 
 
-# Attention Sequence to Sequence Modelクラス
 class AttSeq2Seq(object):
-    def __init__(self, vocab_size, embed_size, hidden_size):
+    def __init__(self, vocab_size, embed_size, hidden_size, batch_size):
         """
-        Attention + Seq2Seqのインスタンス化
+        Sequence to Sequence with Attention Model
         :param vocab_size: 語彙数のサイズ
         :param embed_size: 単語ベクトルのサイズ
         :param hidden_size: 隠れ層のサイズ
         """
         super(AttSeq2Seq, self).__init__()
+        # self.inputs = Input(shape=(vocab_size,))
+
         self.f_encoder = LSTMEncoder(vocab_size, embed_size, hidden_size)  # Forward Encoder
         self.b_encoder = LSTMEncoder(vocab_size, embed_size, hidden_size)  # Backward Encoder
-        self.attention = Attention(hidden_size)  # Attention Model
-        self.decoder = AttLSTMDecoder(vocab_size, embed_size, hidden_size)  # Decoder
+        self.attention = Attention(hidden_size, batch_size)
+        self.decoder = AttLSTMDecoder(vocab_size, embed_size, hidden_size)
 
         self.vocab_size = vocab_size
         self.embed_size = embed_size
         self.hidden_size = hidden_size
-        # 順向きのEncoderの中間ベクトルを保存する用
-        self.fs = []
-        # 逆向きのEncoderの中間ベクトルを保存する用
-        self.bs = []
+        self.fs = []  # 順向きのEncoderの中間ベクトル
+        self.bs = []  # 逆向きのEncoderの中間ベクトル
+        self.c = backend.zeros((batch_size, self.hidden_size))  # 内部メモリ
+        self.h = backend.zeros((batch_size, self.hidden_size))  # 中間ベクトル
 
     def encode(self, words, batch_size):
         """
@@ -160,26 +162,25 @@ class AttSeq2Seq(object):
         :param batch_size: ミニバッチのサイズ
         :return:
         """
-        # 内部メモリ、中間ベクトルの初期化
-        # xp = cuda.get_array_module(words)
-        # 発話リスト内の単語をrowで分割
-        words = [np.array(row, dtype='int32') for row in words]
-        c = np.zeros((batch_size, self.hidden_size), dtype='float32')
-        h = np.zeros((batch_size, self.hidden_size), dtype='float32')
+        # 発話リスト内の単語をrowで分割して中身のみをnumpy arrayに変換
+        words = [np.array(row, dtype='int32') for row in words]  # TODO: wordsがinput ???
+        c = backend.zeros((batch_size, self.hidden_size))
+        h = backend.zeros((batch_size, self.hidden_size))
+
         # 順向きのEncoderの計算
         for w in words:
-            c, h = self.f_encoder(w, c, h)
+            c, h = self.f_encoder(self.inputs(w), c, h)  # TODO: ここのw が，Inputになる！?
             self.fs.append(h)  # 計算された中間ベクトルを記録
-        # 内部メモリ、中間ベクトルの初期化
-        c = np.zeros((batch_size, self.hidden_size), dtype='float32')
-        h = np.zeros((batch_size, self.hidden_size), dtype='float32')
+
+        c = backend.zeros((batch_size, self.hidden_size))
+        h = backend.zeros((batch_size, self.hidden_size))
         # 逆向きのEncoderの計算
         for w in reversed(words):
-            c, h = self.b_encoder(w, c, h)
+            c, h = self.b_encoder(w, c, h)  # TODO: ここのw が，Inputになる！?
             self.bs.insert(0, h)  # 計算された中間ベクトルを記録
-        # 内部メモリ、中間ベクトルの初期化
-        self.c = np.zeros((batch_size, self.hidden_size), dtype='float32')
-        self.h = np.zeros((batch_size, self.hidden_size), dtype='float32')
+
+        self.c = backend.zeros((batch_size, self.hidden_size))
+        self.h = backend.zeros((batch_size, self.hidden_size))  # TODO: maybe h ???
 
     def decode(self, w):
         """
@@ -190,13 +191,3 @@ class AttSeq2Seq(object):
         att_f, att_b = self.attention(self.fs, self.bs, self.h)
         t, self.c, self.h = self.decoder(w, self.c, self.h, att_f, att_b)
         return t
-
-    def reset(self):
-        """
-        インスタンス変数を初期化する
-        Encoderの中間ベクトルを記録するリストの初期化
-        """
-        self.fs = []
-        self.bs = []
-        # 勾配の初期化
-        self.zerograds()
--- train.py
+++ train.py
@@ -1,30 +1,31 @@
 # coding: utf-8
 
-import os
-from glob import glob
-from os.path import relpath, splitext
+# import os
+# from glob import glob
+# from os.path import relpath, splitext
 
 import tensorflow as tf
 # from tensorflow.python import keras
 from tensorflow.python.keras import Model
+from tensorflow.python.keras import backend
 import numpy as np
 
 from model import AttSeq2Seq
 
 
 flags = tf.flags
-flags.DEFINE_integer('-e', '--epoch', 200, "Number of epoch")
-flags.DEFINE_bool('-r', '--resume', False, "Resume mode if this flag is set")
-flags.DEFINE_integer('-b', '--batchsize', 20, "Number of batch size")
-flags.DEFINE_integer('-es', '--embed_size', 100, "Number of embed(vector) size")
-flags.DEFINE_integer('-n', '--n_hidden', 100, "Number of hidden units")
+flags.DEFINE_integer('epochs', 200, "Number of epochs")
+flags.DEFINE_bool('resume', False, "Resume mode if this flag is set")
+flags.DEFINE_integer('batch_size', 20, "Number of batch size")
+flags.DEFINE_integer('embed_size', 100, "Number of embed(vector) size")
+flags.DEFINE_integer('hidden_size', 100, "Number of hidden units")
 # デコードはEOSが出力されれば終了する、出力されない場合の最大出力語彙数
-flags.DEFINE_integer('-d', '--decode_max_size', 15, "Number of decode max size")
-flags.DEFINE_string('-v', '--vocab_file', './data/vocab.txt', "Directory to vocab file")
-flags.DEFINE_string('-i', '--infile', './data/dataid.txt', "Directory to id file")
-flags.DEFINE_string('-o', '--out', './result/', "Directory to output the result")
-flags.DEFINE_integer('-s', '--select', 0, "Select npz file.")
-flags.DEFINE_bool('-g', '--gpu', False, 'GPU mode if this flag is set')
+flags.DEFINE_integer('decode_max_size', 15, "Number of decode max size")
+flags.DEFINE_string('vocab_file', './data/vocab.txt', "Directory to vocab file")
+flags.DEFINE_string('infile', './data/dataid.txt', "Directory to id file")
+flags.DEFINE_string('out', './result/', "Directory to output the result")
+flags.DEFINE_integer('select', 0, "Select npz file.")
+flags.DEFINE_bool('gpu', False, 'GPU mode if this flag is set')
 FLAGS = flags.FLAGS
 
 
@@ -48,65 +49,54 @@ def load_ids():
     return queries, responses
 
 
-def batch_ids(ids, sentence_type):  # TODO: 0を補填する
+def batch_ids(ids, sentence_type):  # TODO: 0を補填する (<eos>)
     if sentence_type == "query":  # queryの場合は前方に-1を補填する
         if len(ids) > FLAGS.decode_max_size:  # ミニバッチ単語サイズになるように先頭から削る
             del ids[0:len(ids) - FLAGS.decode_max_size]
         else:  # ミニバッチ単語サイズになるように前方に付け足す
-            ids = ([-1] * (FLAGS.decode_max_size - len(ids))) + ids
+            # ids = ([-1] * (FLAGS.decode_max_size - len(ids))) + ids
+            ids = ([0] * (FLAGS.decode_max_size - len(ids))) + ids
     elif sentence_type == "response":  # responseの場合は後方に-1を補填する
         if len(ids) > FLAGS.decode_max_size:  # ミニバッチ単語サイズになるように末尾から削る
             del ids[FLAGS.decode_max_size:]
         else:  # ミニバッチ単語サイズになるように後方に付け足す
-            ids = ids + ([-1] * (FLAGS.decode_max_size - len(ids)))
+            # ids = ids + ([-1] * (FLAGS.decode_max_size - len(ids)))
+            ids = ids + ([0] * (FLAGS.decode_max_size - len(ids)))
     return ids
 
 
-def main():
+def main(_):
     print('GPU: {}'.format(FLAGS.gpu))
-    print('# Minibatch-size: {}'.format(FLAGS.batchsize))
+    print('# Minibatch-size: {}'.format(FLAGS.batch_size))
     print('# embed_size: {}'.format(FLAGS.embed_size))
-    print('# n_hidden: {}'.format(FLAGS.n_hidden))
-    print('# epoch: {}'.format(FLAGS.epoch))
+    print('# hidden_size: {}'.format(FLAGS.hidden_size))
+    print('# epoch: {}'.format(FLAGS.epochs))
     print()
 
     # 単語辞書の読み込み
     vocab = load_vocab()
-    model = AttSeq2Seq(vocab_size=len(vocab),
-                       embed_size=FLAGS.embed_size,
-                       hidden_size=FLAGS.n_hidden)
-    # 学習用データを読み込む
+    seq2seq = AttSeq2Seq(vocab_size=len(vocab),
+                         embed_size=FLAGS.embed_size,
+                         hidden_size=FLAGS.hidden_size,
+                         batch_size=FLAGS.batch_size)
+
+    inputs = seq2seq.inputs
+    outputs = seq2seq.decode(backend.zeros((FLAGS.batch_size,)))
+
+    # Load train data
     queries, responses = load_ids()
-    if FLAGS.resume:
-        if FLAGS.select == 0:
-            # 最新のモデルデータを使用する．
-            files = [splitext(relpath(s, FLAGS.out))[0] for s in glob(FLAGS.out + "*.h5")]
-            num = max(list(map(int, files)))
-        else:
-            # 指定のモデルデータを使用する．
-            num = FLAGS.select
-        hdf5 = FLAGS.out + str(num) + ".h5"
-        print("Resume training from", hdf5)
-    else:
-        try:
-            os.mkdir(FLAGS.out)
-        except:
-            pass
-        print("Train")
-        hdf5 = None
-
-    # if hdf5 is not None:
-    #     model = keras.models.load_model(hdf5)
-
     train_queries = np.array(queries)
     train_responses = np.array(responses)
-    # teacher_num = min(len(train_queries), len(train_responses))
 
-    model = Model(inputs=train_queries, outputs=train_responses)
+    model = Model(inputs=inputs, outputs=outputs)
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
-    model.fit(train_queries, train_responses, batch_size=FLAGS.batchsize, epochs=FLAGS.epoch)
+    model.fit(train_queries,
+              train_responses,
+              batch_size=FLAGS.batch_size,
+              epochs=FLAGS.epochs)
+
     model.save(FLAGS.out + str(FLAGS.epoch) + '.h5')
 
 
