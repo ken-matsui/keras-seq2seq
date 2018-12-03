535e0ab78b3544db443d5ef3116a262234ef16ee
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-12-03 13:37:16 +0900

    fix

--- docs/app.js
+++ docs/app.js
@@ -3,14 +3,13 @@ let model;
 tf.loadModel('./model/model.json')
     .then(pretrainedModel => {
         document.getElementById('inference-button').classList.remove('is-loading');
-        pretrainedModel.summary();
         model = pretrainedModel;
     })
     .catch(error => {
         console.log(error);
     });
 
-function getAccuracyScores(imageData) {
+function getResponseSentence(imageData) {
     const score = tf.tidy(() => {
         // convert to tensor (shape: [width, height, channels])
         const channels = 1; // grayscale
@@ -27,17 +26,5 @@ function getAccuracyScores(imageData) {
 
 function inference() {
     console.log(document.getElementById("input_text").value);
-
-    // const imageData = getImageData();
-    // const accuracyScores = getAccuracyScores(imageData);
-    // const maxAccuracy = accuracyScores.indexOf(Math.max.apply(null, accuracyScores));
-    // const elements = document.querySelectorAll(".accuracy");
-    // elements.forEach(el => {
-    //     el.parentNode.classList.remove('is-selected');
-    //     const rowIndex = Number(el.dataset.rowIndex);
-    //     if (maxAccuracy === rowIndex) {
-    //         el.parentNode.classList.add('is-selected');
-    //     }
-    //     el.innerText = accuracyScores[rowIndex];
-    // })
+    // getResponseSentence();
 }
